101e9f582b9fa760580b4e366e68294d22939d78
zhoudanjie <zhoudanjie@linhuiba.com>
2019-02-27 10:26:58 +0800

    提交iOS代码

---
+++ VideoRecordDemo/VideoRecordDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VideoRecordDemo
+//
+//  Created by 邻汇吧 on 2019/2/20.
+//  Copyright © 2019年 邻汇吧. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VideoRecordDemo/VideoRecordDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VideoRecordDemo
+//
+//  Created by 邻汇吧 on 2019/2/20.
+//  Copyright © 2019年 邻汇吧. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
